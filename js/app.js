8b5f36caee2bfae35019a6268a782c6f3c5df16b
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2021-07-09 10:23:07 +0200

    tentative ouv/ferm modale formulaire accessible

--- js/app.js
+++ js/app.js
@@ -7,22 +7,37 @@ photographe
 
 document.addEventListener('DOMContentLoaded', () => {
 const declencheurs = document.querySelectorAll('[aria-haspopup="dialog"]');
-const doc = document.querySelector('.js-document');
+const docGeneral = document.querySelector('.js-document');
 
 const ouvrir = function(dialog){
     dialog.setAttribute('aria-hidden', false);
-    document.setAttribute('aria-hidden', true);
+    docGeneral.setAttribute('aria-hidden', true);
+};
+
+const fermer = function(dialog){
+    dialog.setAttribute('aria-hidden', true);
+    docGeneral.setAttribute('aria-hidden', false);
 };
 
 declencheurs.forEach((declencheur) => {
 const dialog = document.getElementById(declencheur.getAttribute('aria-controls'));
+const rejetDeclencheurs = dialog.querySelectorAll('[data-dismiss]');
 
 //ouvrir dialogue
-
-declencheur.addEventListener('click,' (e) => {
+declencheur.addEventListener('click', (e) => {
     e.preventDefault();
 
     ouvrir(dialog);
-    });
 });
+
+//fermer diaolgue
+rejetDeclencheurs.forEach((rejetDeclencheur) => {
+    const rejetDialog = document.getElementById(rejetDeclencheur.dataset.dismiss);
+
+    rejetDeclencheur.addEventListener('click', (e) =>{
+        if (e.target === dialog){
+            close(dialog);
+        }
+        });
+    });
 });
