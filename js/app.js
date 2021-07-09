61a8239aa02900877da4a1b7c4c82b74d66f32c3
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2021-07-09 10:30:04 +0200

    remplacement dialog par modale-formulaire

--- js/app.js
+++ js/app.js
@@ -6,38 +6,44 @@ photographe
 */
 
 document.addEventListener('DOMContentLoaded', () => {
-const declencheurs = document.querySelectorAll('[aria-haspopup="dialog"]');
-const docGeneral = document.querySelector('.js-document');
+  const declencheurs = document.querySelectorAll('[aria-haspopup="modale-formulaire"]')
+  const docGeneral = document.querySelector('.js-document')
 
-const ouvrir = function(dialog){
-    dialog.setAttribute('aria-hidden', false);
-    docGeneral.setAttribute('aria-hidden', true);
-};
+  const ouvrir = function (dialog) {
+    dialog.setAttribute('aria-hidden', false)
+    docGeneral.setAttribute('aria-hidden', true)
+  }
 
-const fermer = function(dialog){
-    dialog.setAttribute('aria-hidden', true);
-    docGeneral.setAttribute('aria-hidden', false);
-};
+  const fermer = function (dialog) {
+    dialog.setAttribute('aria-hidden', true)
+    docGeneral.setAttribute('aria-hidden', false)
+  }
 
-declencheurs.forEach((declencheur) => {
-const dialog = document.getElementById(declencheur.getAttribute('aria-controls'));
-const rejetDeclencheurs = dialog.querySelectorAll('[data-dismiss]');
+  declencheurs.forEach((declencheur) => {
+    const dialog = document.getElementById(declencheur.getAttribute('aria-controls'))
+    const rejetDeclencheurs = dialog.querySelectorAll('[data-dismiss]')
 
-//ouvrir dialogue
-declencheur.addEventListener('click', (e) => {
-    e.preventDefault();
+    // ouvrir dialogue
+    declencheur.addEventListener('click', (e) => {
+      e.preventDefault()
 
-    ouvrir(dialog);
-});
+      ouvrir(dialog)
+    })
 
-//fermer diaolgue
-rejetDeclencheurs.forEach((rejetDeclencheur) => {
-    const rejetDialog = document.getElementById(rejetDeclencheur.dataset.dismiss);
+    // fermer diaolgue
+    rejetDeclencheurs.forEach((rejetDeclencheur) => {
+      const rejetDialog = document.getElementById(rejetDeclencheur.dataset.dismiss)
 
-    rejetDeclencheur.addEventListener('click', (e) =>{
-        if (e.target === dialog){
-            close(dialog);
-        }
-        });
-    });
-});
+      rejetDeclencheur.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        fermer(rejetDialog)
+      })
+    })
+    window.addEventListener('click', (e) => {
+      if (e.target === dialog) {
+        close(dialog)
+      }
+    })
+  })
+})
