41cf9a1a86e2331c586b40f75087466dccd377e0
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2021-07-24 17:11:03 +0200

    recuperation nom 1

--- js/test.js
+++ js/test.js
@@ -1,8 +1,11 @@
+
 // éléments du DOM
-// const corpsBody = document.querySelector('.js-page')
+const corpsBody = document.querySelector('.js-page')
 const corpsContenuPage = document.querySelector('.js-document')
 
-// récupération des données JSON
+
+
+// essai 2
 async function recupData () {
   const reponse = await fetch('js/data/fisheyedata.json')
   const data = await reponse.json()
@@ -12,8 +15,8 @@ async function recupData () {
   console.log('coucou depuis fonction recupData')
   console.log(data.photographers)
 
-  for (let i = 0; i < data.photographers.lenght; i++) {
-    newTitre.innerText = 'nom' + data.photographers[i].name
+  for (let i = 0; i < data.photographers.length; i++) {
+    newTitre.innerText = 'nom: ' + data.photographers[i].name
     newLi.appendChild(newTitre)
     corpsContenuPage.appendChild(newLi)
     console.log('coucou depuis la boucle data json')
@@ -21,25 +24,47 @@ async function recupData () {
 }
 
 recupData().catch(erreur => {
-  console.log('Erreur dans le chargement des données. ' + ('( ') + erreur  + (' )'))
+  console.log('Erreur dans le chargement des données. ' + ('( ') + erreur + (' )'))
 })
 
+// essai 1
+// const getPhotographes = async () => {
+//   try {
+//     const reponse = await fetch('js/data/fisheyedata.json')
+//     if (reponse.ok) {
+//       const data = await reponse.json()
+//       console.log('depuis getPhotographes')
+//       console.log(data)
+//     } else {
+//       console.error('erreur signalée:', reponse.status)
+//     }
+//   } catch (erreur) {
+//     console.log(erreur)
+//   }
+// }
+
+// getPhotographes()
+
+// essai 3
 // fetch('js/data/fisheyedata.json')
 //   .then(reponse => reponse.json())
 //   .then(data => {
-//     // console.log(data.photographers)
-
-//     for (let i = 0; i < data.photographers.lenght; i++) {
-//       console.log('coucou')
-//       let photographeTableau = []
-//       const newLi = document.createElement('li')
-//       const newTitre = document.createElement('h2')
-
-//       newTitre.innerText = 'nom' + data.photographers[i].name
-//       newLi.appendChild(newTitre)
-//       corpsContenuPage.appendChild(newLi)
-//     }
+//     appendData(data)
 //   })
 //   .catch(erreur => {
 //     console.log('Erreur dans le chargement des données: ' + erreur)
 //   })
+// function appendData (data) {
+//   const corpsContenuPage = document.querySelector('.js-document')
+//   console.log('coucou depuis appendData')
+
+//   for (let i = 0; i < data.length; i++) {
+//     console.log('coucou depuis la boucle appendData')
+//     const newLi = document.createElement('li')
+//     const newTitre = document.createElement('h2')
+//     newTitre.innerText = 'nom' + data[i].photographers.name
+//     newLi.appendChild(newTitre)
+
+//     corpsContenuPage.appendChild(newLi)
+//   }
+// }
