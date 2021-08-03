f8435a2c2f8881ca2c952e6120b4898be0cc41dd
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2021-08-03 12:54:15 +0200

    fiches Photographe avec map

--- js/indextest.js
+++ js/indextest.js
@@ -10,7 +10,6 @@
 // éléments du DOM
 const corpsPage = document.querySelector('.js-page')
 const corpsContenuPage = document.querySelector('.js-document')
-// const photographesGalerie = document.querySelector('.photographes-galerie')
 
 // 1 - récupération des données JSON
 const apiUrl = 'js/data/fisheyedata.json'
@@ -27,7 +26,6 @@ const jsonData = async () => {
 // 2 - chargement des données JSON au chargement de la page
 const chargementData = async () => {
   const data = await jsonData()
-  console.log(data)
   fichePhotographe(data)
 }
 document.addEventListener('DOMContentLoaded', chargementData)
@@ -36,7 +34,7 @@ document.addEventListener('DOMContentLoaded', chargementData)
 // const dataPhotographes = jsonData()
 // console.log(dataPhotographes)
 
-// 4 - Générer les éléments HTML
+// 4 - Générer les éléments HTML individuellement
 const templateHeader = () => {
   const header = document.createElement('header')
   header.classList.add('banniere')
@@ -52,56 +50,63 @@ const templateHeader = () => {
         <h1 tabindex="0">Nos photographes</h1>
 `
   corpsPage.appendChild(header)
+  console.log('templateHeader généré')
 }
 
 const templateMain = () => {
   const main = document.createElement('main')
   main.classList.add('js-document')
   main.setAttribute('id', 'contenu-principal')
-  main.innerHTML = `
-  <ul class="photographes-galerie">
-  </ul>`
+  // main.innerHTML = `
+  // <main id="contenu-principal" class="js-document">
+  // <ul class="photographes-galerie">
+  // </ul>
+  // </main>`
   corpsPage.appendChild(main)
   // ajouter les fiches photographes (?)
   // main.appendChild(fichePhotographe)
+  console.log('templateMain généré')
 }
 
 const fichePhotographe = (data) => {
+  const photographesGalerie = document.createElement('ul')
+  photographesGalerie.classList.add('photographes-galerie')
+  corpsContenuPage.appendChild(photographesGalerie)
   const listePhotographes = data.photographers
-  // const photographesGalerie = document.createElement('ul')
-  // photographesGalerie.classList.add('photographes-galerie')
-  // corpsContenuPage.appendChild(photographesGalerie)
 
   listePhotographes.forEach((data) => {
-    console.log(`fiche de ${data.name}`)
-    const fichePhotographe = document.createElement('li')
-    fichePhotographe.classList.add('photographe-profil')
-    fichePhotographe.innerHtml = `
-      <a href="photographer${data.id}.html">
-                      <img class="vignette" src="resources/img/photographers/IDphotos/${data.name}.jpg" alt=" "/>
-                      <h2 class="nom">${data.name}</h2>
-                  </a>
-                  <div tabindex="0">
-                      <p class="localisation">${data.photographers.city}, ${data.photographers.country}</p>
-                      <p class="accroche">${data.tagline}</p>
-                      <span class="tarif">${data.price}€</span><span class="tarif" aria-label="par jour">/jour</span>
-                  </div>
-                  <ul class="nav-par-tag" >
-                      <li class="tag-entree"><a href="#"><span aria-label="hashtag">#</span>Portrait</a></li>
-                      <li class="tag-entree"><a href="#"><span aria-label="hashtag">#</span>Art</a></li>
-                  </ul>`
-    console.log(fichePhotographe)
+    console.log(`génération fiche de ${data.name}`)
+    // const fichePhotographe = document.createElement('li')
+    // fichePhotographe.classList.add('photographe-profil')
+    // photographesGalerie.appendChild(fichePhotographe)
+    // fichePhotographe.innerHtml = `
+    //   <a href="photographer${data.id}.html">
+    //                   <img class="vignette" src="resources/img/photographers/IDphotos/${data.name}.jpg" alt=" "/>
+    //                   <h2 class="nom">${data.name}</h2>
+    //               </a>
+    //               <div tabindex="0">
+    //                   <p class="localisation">${data.photographers.city}, ${data.photographers.country}</p>
+    //                   <p class="accroche">${data.tagline}</p>
+    //                   <span class="tarif">${data.price}€</span><span class="tarif" aria-label="par jour">/jour</span>
+    //               </div>
+    //               <ul class="nav-par-tag" >
+    //                   <li class="tag-entree"><a href="#"><span aria-label="hashtag">#</span>Portrait</a></li>
+    //                   <li class="tag-entree"><a href="#"><span aria-label="hashtag">#</span>Art</a></li>
+    //               </ul>`
   })
 }
 
+// 5 - Générer entièrement l'index
 function templateIndex () {
   templateHeader()
   templateMain()
   fichePhotographe()
+  console.log('Index généré')
 }
 templateIndex()
 
-// tentatives
+// tentatives:
+
 // const tableauPhotographes = (data) => {
 //   return data.photographers
 // }
